9a31815542a4fb462fa5bee8e7365dace91f05e2
shane <kittichoteshane@gmail.com>
2023-02-27 01:25:35 +0700

    Fix dependendies in server

--- packages/server/src/resources/analytics/analytics.module.ts
+++ packages/server/src/resources/analytics/analytics.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { AnalyticsResolver } from './analytics.resolver';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
   providers: [AnalyticsResolver, AnalyticsService],
+  imports: [UsersModule],
 })
 export class AnalyticsModule {}
